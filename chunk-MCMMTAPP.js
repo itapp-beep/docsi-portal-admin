5523c1ed5b112bf723d134ec6e9935651d2d9995
KAHMIADEV <kahmiadev@Maquina-virtual-de-KAHMIADEV.local>
2026-09-20 20:14:45 -0400

    Auto-generated commit

---
+++ chunk-MCMMTAPP.js
@@ -0,0 +1,1 @@
+var e={pendiente:"Pendiente",confirmada:"Confirmada",cancelada:"Cancelada",completada:"Completada",no_asistio:"No asisti\xF3"},i=[{valor:1,nombre:"Lunes"},{valor:2,nombre:"Martes"},{valor:3,nombre:"Mi\xE9rcoles"},{valor:4,nombre:"Jueves"},{valor:5,nombre:"Viernes"},{valor:6,nombre:"S\xE1bado"},{valor:7,nombre:"Domingo"}];export{e as a,i as b};
